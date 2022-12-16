18ad24269284aaf42ef8c1b8d84d89f33e658a5b
xopher <chriswwl95@gmail.com>
2022-12-16 14:18:40 +1100

    add desktop, and mobile sites

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react"
 import {useRouter} from "next/router"
 import {E, updateState} from "/utils/state"
 import "focus-visible/dist/focus-visible"
-import {Box, Container, HStack, Heading, Image, Link, Text, VStack} from "@chakra-ui/react"
+import {Box, Container, HStack, Heading, Image, Link, SimpleGrid, Text, VStack} from "@chakra-ui/react"
 import NextLink from "next/link"
 import NextHead from "next/head"
 
@@ -34,6 +34,7 @@ useEffect(() => {
 })
 return (
 <Box>
+<Box sx={{"display": ["none", "block", "block", "block"]}}>
 <Container centerContent={true}>
 <VStack spacing="1.5em"
 sx={{"fontSize": "2em", "paddingTop": "10%"}}>
@@ -41,7 +42,7 @@ sx={{"fontSize": "2em", "paddingTop": "10%"}}>
 {`Welcome to Pycone site!`}</Heading>
 <Image src="/images/me.jpg"
 sx={{"borderRadius": "100%", "width": 300}}/>
-<Box sx={{"paddingTop": 10, "width": "87%"}}>
+<Box sx={{"paddingTop": 10, "width": "70%", "direction": "column"}}>
 <Text>
 {`Welcome to my Python web app! I am from Malaysia born in Kota Kinabalu, Sabah. I graduated as Mechanical Engineer from Iowa State University in 2018 and currently doing Master in Business Informatics in University of Canberra.`}</Text>
 <Box sx={{"height": 10}}/>
@@ -52,35 +53,79 @@ sx={{"borderRadius": "100%", "width": 300}}/>
 {`I also love learning new stuff and reading books. Anyway, I hope you enjoy my website, you may reach me in one of the medium below!`}</Text></Box>
 <HStack spacing="10"
 sx={{"padding": 150}}>
-<NextLink href="https://github.com/xopherw"
-passHref={true}>
+<NextLink passHref={true}
+href="https://github.com/xopherw">
 <Link sx={{"padding": "0.10em", "width": 75}}>
 <Image src="/icons/github.png"/></Link></NextLink>
-<NextLink href="https://www.linkedin.com/in/chriswwl95/"
-passHref={true}>
+<NextLink passHref={true}
+href="https://www.linkedin.com/in/chriswwl95/">
 <Link sx={{"padding": "0.10em", "width": 75}}>
 <Image src="/icons/linkedin.png"/></Link></NextLink>
-<NextLink href="mailto:chriswwl95@gmail.com"
-passHref={true}>
+<NextLink passHref={true}
+href="mailto:chriswwl95@gmail.com">
 <Link sx={{"padding": "0.10em", "width": 75}}>
 <Image src="/icons/gmail.png"/></Link></NextLink>
-<NextLink href="https://medium.com/@chriswwl95"
-passHref={true}>
+<NextLink passHref={true}
+href="https://medium.com/@chriswwl95">
 <Link sx={{"padding": "0.10em", "width": 75}}>
 <Image src="/icons/medium.png"/></Link></NextLink>
-<NextLink href="https://unsplash.com/@xopher"
-passHref={true}>
+<NextLink passHref={true}
+href="https://unsplash.com/@xopher">
 <Link sx={{"padding": "0.10em", "width": 75}}>
 <Image src="/icons/unsplash.png"/></Link></NextLink>
-<NextLink href="http://xopherw.duckdns.org"
-passHref={true}>
+<NextLink passHref={true}
+href="http://xopherw.duckdns.org">
 <Link sx={{"padding": "0.10em", "width": 75}}>
-<Image src="/icons/archive.png"/></Link></NextLink></HStack></VStack></Container>
+<Image src="/icons/archive.png"/></Link></NextLink></HStack></VStack></Container></Box>
+<Box sx={{"display": ["block", "none", "none", "none"]}}>
+<Container>
+<VStack spacing="1.5em"
+sx={{"fontSize": "2em", "paddingTop": "10%"}}>
+<Heading>
+{`Welcome to Pycone site!`}</Heading>
+<Image src="/images/me.jpg"
+sx={{"borderRadius": "100%", "width": 300}}/>
+<Box sx={{"paddingTop": 10, "fontSize": 18, "width": 250}}>
+<Text>
+{`Welcome to my Python web app! I am from Malaysia born in Kota Kinabalu, Sabah. I graduated as Mechanical Engineer from Iowa State University in 2018 and currently doing Master in Business Informatics in University of Canberra.`}</Text>
+<Box sx={{"height": 10}}/>
+<Text>
+{`I have always been passionate about technology. This is an example of what Pycone framework can achieve with just Python language! In my free time I like to work on my personal coding project mainly with Ptyhon.`}</Text>
+<Box sx={{"height": 10}}/>
+<Text>
+{`I also love learning new stuff and reading books. Anyway, I hope you enjoy my website, you may reach me in one of the medium below!`}</Text></Box>
+<SimpleGrid spacing="10"
+columns={[3]}
+sx={{"padding": 15}}>
+<NextLink passHref={true}
+href="https://github.com/xopherw">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/github.png"/></Link></NextLink>
+<NextLink passHref={true}
+href="https://www.linkedin.com/in/chriswwl95/">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/linkedin.png"/></Link></NextLink>
+<NextLink passHref={true}
+href="mailto:chriswwl95@gmail.com">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/gmail.png"/></Link></NextLink>
+<NextLink passHref={true}
+href="https://medium.com/@chriswwl95">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/medium.png"/></Link></NextLink>
+<NextLink passHref={true}
+href="https://unsplash.com/@xopher">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/unsplash.png"/></Link></NextLink>
+<NextLink passHref={true}
+href="http://xopherw.duckdns.org">
+<Link sx={{"padding": "0.10em", "width": 10}}>
+<Image src="/icons/archive.png"/></Link></NextLink></SimpleGrid></VStack></Container></Box>
 <NextHead>
 <title>{`Pynecone App`}</title>
-<meta name="description"
-content="favicon.ico"/>
-<meta property="og:image"
-content="A Pynecone app."/></NextHead></Box>
+<meta content="favicon.ico"
+name="description"/>
+<meta content="A Pynecone app."
+property="og:image"/></NextHead></Box>
 )
 }
--- first_app/first_app.py
+++ first_app/first_app.py
@@ -2,15 +2,7 @@
 from pcconfig import config
 
 import pynecone as pc
-
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-
-box_style = {
-    "padding":"0.10em",
-    "width" : 75,
-
-}
-
+# import desktop as dtop
 
 
 class State(pc.State):
@@ -19,74 +11,165 @@ class State(pc.State):
     pass
 
 
-def index():
-    return pc.box(
+box_style = {
+    "padding": "0.10em",
+    "width": 75,
+}
+
+mobile_box_style = {
+    
+    "padding": "0.10em",
+    "width": 10,
+
+}
+
+def desktop():
+    return pc.tablet_and_desktop(
         pc.container(
             pc.vstack(
                 pc.heading(
                     "Welcome to Pycone site!",
-                    ),
+                ),
 
                 pc.image(
-                    src="/images/me.jpg", 
+                    src="/images/me.jpg",
                     border_radius="100%",
                     width=300,
-                    ),                
-                    
+                ),
+
                 pc.box(
                     pc.text("Welcome to my Python web app! I am from Malaysia born in Kota Kinabalu, Sabah. I graduated as Mechanical Engineer from Iowa State University in 2018 and currently doing Master in Business Informatics in University of Canberra."),
                     pc.box(height=10),
                     pc.text("I have always been passionate about technology. This is an example of what Pycone framework can achieve with just Python language! In my free time I like to work on my personal coding project mainly with Ptyhon."),
                     pc.box(height=10),
-                    pc.text("I also love learning new stuff and reading books. Anyway, I hope you enjoy my website, you may reach me in one of the medium below!"),
+                    pc.text(
+                        "I also love learning new stuff and reading books. Anyway, I hope you enjoy my website, you may reach me in one of the medium below!"),
                     padding_top=10,
-                    width="87%"
-                    ),
-                
+                    width="70%",
+                    direction="column",
+                ),
+
                 pc.hstack(
                     pc.link(
                         pc.image(src="/icons/github.png"),
-                        href="https://github.com/xopherw", 
+                        href="https://github.com/xopherw",
                         style=box_style
-                        ),
+                    ),
                     pc.link(
                         pc.image(src="/icons/linkedin.png"),
                         href="https://www.linkedin.com/in/chriswwl95/",
                         style=box_style
-                        ),
+                    ),
                     pc.link(
                         pc.image(src="/icons/gmail.png"),
                         href="mailto:chriswwl95@gmail.com",
                         style=box_style,
-                        ),
+                    ),
                     pc.link(
                         pc.image(src="/icons/medium.png"),
                         href="https://medium.com/@chriswwl95",
                         style=box_style,
-                        ),
+                    ),
                     pc.link(
                         pc.image(src="/icons/unsplash.png"),
                         href="https://unsplash.com/@xopher",
                         style=box_style,
-                        ),
+                    ),
                     pc.link(
                         pc.image(src="/icons/archive.png"),
                         href="http://xopherw.duckdns.org",
                         style=box_style,
-                        ),
+                    ),
                     padding=150,
                     spacing="10"
-                    ),            
+                ),
 
                 spacing="1.5em",
                 font_size="2em",
                 padding_top="10%",
-                
+
             ),
             center_content=True
         )
     )
-    
+
+
+def mobile():
+    return pc.mobile_only(
+        pc.container(
+            pc.vstack(
+                pc.heading(
+                    "Welcome to Pycone site!",
+                ),
+
+                pc.image(
+                    src="/images/me.jpg",
+                    border_radius="100%",
+                    width=300,
+                ),
+
+                pc.box(
+                    pc.text("Welcome to my Python web app! I am from Malaysia born in Kota Kinabalu, Sabah. I graduated as Mechanical Engineer from Iowa State University in 2018 and currently doing Master in Business Informatics in University of Canberra."),
+                    pc.box(height=10),
+                    pc.text("I have always been passionate about technology. This is an example of what Pycone framework can achieve with just Python language! In my free time I like to work on my personal coding project mainly with Ptyhon."),
+                    pc.box(height=10),
+                    pc.text(
+                        "I also love learning new stuff and reading books. Anyway, I hope you enjoy my website, you may reach me in one of the medium below!"),
+                    padding_top=10,
+                    font_size=18,
+                    width=250
+                ),
+
+                pc.responsive_grid(
+                    pc.link(
+                        pc.image(src="/icons/github.png"),
+                        href="https://github.com/xopherw",
+                        style=mobile_box_style
+                    ),
+                    pc.link(
+                        pc.image(src="/icons/linkedin.png"),
+                        href="https://www.linkedin.com/in/chriswwl95/",
+                        style=mobile_box_style
+                    ),
+                    pc.link(
+                        pc.image(src="/icons/gmail.png"),
+                        href="mailto:chriswwl95@gmail.com",
+                        style=mobile_box_style,
+                    ),
+                    pc.link(
+                        pc.image(src="/icons/medium.png"),
+                        href="https://medium.com/@chriswwl95",
+                        style=mobile_box_style,
+                    ),
+                    pc.link(
+                        pc.image(src="/icons/unsplash.png"),
+                        href="https://unsplash.com/@xopher",
+                        style=mobile_box_style,
+                    ),
+                    pc.link(
+                        pc.image(src="/icons/archive.png"),
+                        href="http://xopherw.duckdns.org",
+                        style=mobile_box_style,
+                    ),
+                    padding=15,
+                    spacing="10",
+                    columns=[3]
+                ),
+
+                spacing="1.5em",
+                font_size="2em",
+                padding_top="10%",
+
+            ),
+        )
+    )
+
+
+def index():
+    return pc.box(
+        desktop(),
+        mobile()
+    )
 
 
 # Add state and page to the app.
