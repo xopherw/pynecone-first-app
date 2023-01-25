5fce07f470d7bd3497cf90b053f76437e4437ec9
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2023-01-25 18:56:25 +1100

    Update pcconfig.py

--- pcconfig.py
+++ pcconfig.py
@@ -3,8 +3,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="first_app",
-    api_url="203.43.150.2:8000"
+    api_url="203.43.150.2:0"
     bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
+#     env=pc.Env.DEV,
 )
